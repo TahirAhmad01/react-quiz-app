f08a553f2341f3c668f646a02067b92f1c05b942
tahir_ahmad <simanto.saikat234@gmail.com>
2021-12-17 20:06:16 +0600

    firebase authenticate add

---
+++ src/component/firebase.js
@@ -0,0 +1,16 @@
+//Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const app = initializeApp({
+  apiKey: "AIzaSyA1sH17u47NFf0IdQeE1xaBrEKDzMCJf74",
+  authDomain: "react-quiz-app-9aa8b.firebaseapp.com",
+  projectId: "react-quiz-app-9aa8b",
+  storageBucket: "react-quiz-app-9aa8b.appspot.com",
+  messagingSenderId: "185574609138",
+  appId: "1:185574609138:web:26898e65a8adb6d2d8406b",
+});
+
+export default app;

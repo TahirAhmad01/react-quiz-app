bb08934625d650590813ec30fcd6f7cf493af64c
tahir_ahmad <simanto.saikat234@gmail.com>
2021-12-19 00:55:11 +0600

    firebase signup added

--- src/component/App.js
+++ src/component/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { AuthProvider } from "../context/authcontext";
 import "../styles/style.css";
 import Layout from "./layout";
 import Home from "./pages/home";
@@ -8,33 +9,35 @@ import Result from "./pages/result";
 import Signup from "./pages/signup";
 
 function App() {
-  return (
-    <Router>
-      <Layout>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
+	return (
+		<Router>
+			<AuthProvider>
+				<Layout>
+					<Switch>
+						<Route exact path="/">
+							<Home />
+						</Route>
 
-          <Route exact path="/login">
-            <Login />
-          </Route>
+						<Route exact path="/login">
+							<Login />
+						</Route>
 
-          <Route exact path="/signup">
-            <Signup />
-          </Route>
+						<Route exact path="/signup">
+							<Signup />
+						</Route>
 
-          <Route exact path="/quiz">
-            <Quiz />
-          </Route>
+						<Route exact path="/quiz">
+							<Quiz />
+						</Route>
 
-          <Route exact path="/result">
-            <Result />
-          </Route>
-        </Switch>
-      </Layout>
-    </Router>
-  );
+						<Route exact path="/result">
+							<Result />
+						</Route>
+					</Switch>
+				</Layout>
+			</AuthProvider>
+		</Router>
+	);
 }
 
 export default App;
